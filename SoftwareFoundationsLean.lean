9330ee8e6147b88000068849595e627f79ae7101
Lean 4 VS Code Extension <>
2025-06-19 20:07:51 +0530

    Initial commit

---
+++ SoftwareFoundationsLean.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `SoftwareFoundationsLean` library.
+-- Import modules here that should be built as part of the library.
+import SoftwareFoundationsLean.Basic
