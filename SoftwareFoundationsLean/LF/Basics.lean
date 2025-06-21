8e69db8e61853e0c396ec2a3e365137967beec97
satiscugcat <23110026@iitgn.ac.in>
2025-06-21 23:52:29 +0530

    Introduction

--- SoftwareFoundationsLean/LF/Basics.lean
+++ SoftwareFoundationsLean/LF/Basics.lean
@@ -1,1 +1,34 @@
-def hello := "world"
+-- - Basics: Functional Programming in LEAN
+
+-- - Introduction 
+
+/-
+    The functional style of programming is founded on simple, everyday
+    mathematical intuitions: If a procedure or method has no side
+    effects, then (ignoring efficiency) all we need to understand
+    about it is how it maps inputs to outputs -- that is, we can think
+    of it as just a concrete method for computing a mathematical
+    function.  This is one sense of the word "functional" in
+    "functional programming."  The direct connection between programs
+    and simple mathematical objects supports both formal correctness
+    proofs and sound informal reasoning about program behavior.
+
+    The other sense in which functional programming is "functional" is
+    that it emphasizes the use of functions as _first-class_ values --
+    i.e., values that can be passed as arguments to other functions,
+    returned as results, included in data structures, etc.  The
+    recognition that functions can be treated as data gives rise to a
+    host of useful and powerful programming idioms.
+
+    Other common features of functional languages include _algebraic
+    data types_ and _pattern matching_, which make it easy to
+    construct and manipulate rich data structures, and _polymorphic
+    type systems_ supporting abstraction and code reuse.  Coq offers
+    all of these features.
+
+    LEAN has been made to be performant both as a programming language
+    and as a theorem prover. The first half of this chapter introduces
+    the most essential functional programming primitives. The second
+    half introduces some basic _tactics_ that can be used to prove
+    properties of the programs that you write within LEAN.
+-/
